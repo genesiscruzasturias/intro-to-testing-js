58adf77cc3e1e3f71c1434088ffa859f778ad6dc
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-08-03 10:29:39 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -1,12 +1,23 @@
-// helloWorld function 
+// helloWorld function
 // function helloWorld() {
 //     return "Hello, World!";
 // }
-
+//
 const helloWorld = function(){
     return "Hello, World!";
 }
 
-function sayHello(){
+function sayHello(input){
+    // return "Hello, Jane!";
+    if(input === "Alex") {
+        return "Hello, " + input + "!";
+    } else if (input === "Jane") {
+        return "Hello, " + input + "!";
+    } else if (input === "Pat")
+        return "Hello, " + input + "!";
+    else {
+        return "Hello, Jane!"
+    }
+}
 
-};
+
